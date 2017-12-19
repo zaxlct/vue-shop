6796e1d58224ba4297d29d7f869647b5502f9927
zaxlct <zaxlct@foxmail.com>
2017-12-19 09:31:58 +0800

    App 组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template lang="pug">
   #app
-    router-view
+    TheNav
+    router-view.view_container
+    TheFooter
 </template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import TheNav from 'components/TheNav'
+import TheFooter from 'components/TheFooter'
+
+@Component({
+  components: {
+    TheNav,
+    TheFooter,
+  },
+})
+export default class App extends Vue {
+
+}
+</script>
 
 <style lang="sass">
 @import '~common/sass/index.sass' // 全局自定义的css样式
+
+#app
+  display: flex
+  min-height: 100vh
+  flex-direction: column
+
+  >.view_container
+    flex: 1
+    background: #f6f6f6
 </style>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class TheNav extends Vue {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
