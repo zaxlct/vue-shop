3a8d6400aa1a8f14f3d1be01504fd266da873117
zaxlct <zaxlct@foxmail.com>
2017-12-19 13:29:07 +0800

    init footer component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.container
+  | Footer
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class TheFooter extends Vue {
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
