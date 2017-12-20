8e30c41a957e6caecacc75f874914e98c910f4cb
zaxlct <zaxlct@foxmail.com>
2017-12-20 22:26:31 +0800

    banner page

--- src/views/home/banner.vue
+++ src/views/home/banner.vue
@@ -1,9 +1,42 @@
 <style lang="sass" scoped>
+h3
+  color: #475669
+  font-size: 14px
+  opacity: 0.75
+  line-height: 150px
+  margin: 0
 
+.el-carousel__item:nth-child(2n)
+    background-color: #99a9bf
+
+.el-carousel__item:nth-child(2n+1)
+    background-color: #d3dce6
+
+// .merits_container
+//   margin-top: 20px
+//   padding: 10px 0
+//   width: 100%
+//   height: 125px
+//   background: #fff
+//   text-align: center
+
+//   >.h2
+//     font-size: 1.2rem
+//     font-weight: 400
+//     padding-bottom: 10px
+
+//   >.merits_list
+//     display: flex
+
+//     >.item
+//       flex: 1
 </style>
 
 <template lang="pug">
-
+.the_banner_container
+  el-carousel(height="582px")
+    el-carousel-item(v-for="item in 4" :key="item")
+      h3 {{ item }}
 </template>
 
 <script>
