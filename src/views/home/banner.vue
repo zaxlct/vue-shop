66cd96715a843fe9d73dcc56e6cb58a521c7801c
zaxlct <zaxlct@foxmail.com>
2017-12-20 21:18:38 +0800

    add banner page

---
+++ src/views/home/banner.vue
@@ -0,0 +1,17 @@
+<style lang="sass" scoped>
+
+</style>
+
+<template lang="pug">
+
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Banner extends Vue {
+
+}
+</script>
